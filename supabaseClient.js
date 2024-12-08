94039d1095691b0d7a3bfc0b661a724148dfc88a
crrenj <juanlantiguajorge@gmail.com>
2024-12-08 20:01:13 +0100

    url et anon

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://votre-url-projet.supabase.co';  // Remplacez par votre URL Supabase
-const SUPABASE_KEY = 'votre-cle-publique-anon';                // Remplacez par votre clé anonyme fournie par Supabase
+const SUPABASE_URL = 'https://jzkxoxacfedtovlcvcfo.supabase.co';  // Remplacez par votre URL Supabase
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6a3hveGFjZmVkdG92bGN2Y2ZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM2ODAwNzMsImV4cCI6MjA0OTI1NjA3M30.GV6N3HFuCLTjBrkorptrk2_dSwXEw80RKeYJEh9AZb4';                // Remplacez par votre clé anonyme fournie par Supabase
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);

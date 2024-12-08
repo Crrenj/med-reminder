4f07c3776551aaf5c3559b8dce0ed8261e446e39
crrenj <juanlantiguajorge@gmail.com>
2024-12-08 17:40:18 +0100

    new backend https

--- docs/script.js
+++ docs/script.js
@@ -2,7 +2,7 @@ const form = document.getElementById('reminderForm');
 const reminderList = document.getElementById('reminderList');
 
 // URL du backend (en local, assurez-vous que le serveur Node.js tourne sur le port 3000)
-const BACKEND_URL = 'http://localhost:3000/api/reminders';
+const BACKEND_URL = 'https://med-reminder-c3j1.onrender.com';
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
